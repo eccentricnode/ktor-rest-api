8c5d753ed1546d396c88ba36f6163582f4c520a1
Austin Johnson <austin_johnson@swissre.com>
2023-12-17 20:09:24 -0700

    htmx setup, broken needs work

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -3,11 +3,19 @@ package com.example.plugins
 import com.example.routes.indexRouting
 import com.example.routes.todosRouting
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
+import jdk.javadoc.internal.doclets.formats.html.markup.Head
 
 fun Application.configureRouting() {
     routing {
+        staticResources("/static", "assets")
         todosRouting()
         indexRouting()
     }
+}
+
+fun Head.commonHead() {
+    title("Htmx-demo")
+    script(src = "/static/htmx.min.js") {}
 }
